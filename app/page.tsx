f6c945566289458c4ebda9eae3ec5ea13a8ce938
idsta.ivandi <idsta.ivandi@xl.co.id>
2024-04-27 14:48:00 +0700

    playaround with fonts

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-       <h3>Hello World</h3>
-      </div>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24"> 
+       <h3 className="text-4xl font-bold">Hello World</h3>
     </main>
   );
 }
